200cf665c4d4e1f70b8874f8bfadfa5c46ba5e0f
Travis Boatright <travis@Traviss-MacBook-Pro.local>
2020-07-19 12:48:43 -0400

    Updates

---
+++ precache-manifest.ba7a592856b80b8934f7063e10387238.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d351fe2478078d02b6b75d37232315ad",
+    "url": "/React-todo-list/index.html"
+  },
+  {
+    "revision": "2d99f4e974002dc32cee",
+    "url": "/React-todo-list/static/css/main.794f42ad.chunk.css"
+  },
+  {
+    "revision": "13378cd698d80046920f",
+    "url": "/React-todo-list/static/js/2.efdbadb3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-todo-list/static/js/2.efdbadb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d99f4e974002dc32cee",
+    "url": "/React-todo-list/static/js/main.6e222925.chunk.js"
+  },
+  {
+    "revision": "badfbd9bd8f63c9e0d31",
+    "url": "/React-todo-list/static/js/runtime-main.0635c952.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-todo-list/static/media/logo.5d5d9eef.svg"
+  }
+]);
